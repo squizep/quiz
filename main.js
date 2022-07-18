9501493963e8066ba1eeba06a333211b2db95f74
squizep <wonderbuf@gmail.com>
2022-07-18 06:10:18 +0300

    yes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const optionElements = document.querySelectorAll('.option');
 
 const question = document.getElementById('question');
 const numberOfQuestion = document.getElementById('number-of-question'),
-	numberOfAllQuestions = document.getElementById('number-of-all-question');
+	numberOfAllQuestions = document.getElementById('number-of-all-questions');
 
 let indexOfQuestion,
 	indexOfPage = 0;
@@ -18,23 +18,23 @@ let score = 0;
 
 const correctAnswer = document.getElementById('correct-answer'),
 	numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'),
-	btnTryAgain = document.getElementById('btn-tey-again');
+	btnTryAgain = document.getElementById('btn-try-again');
 
 const questions = [
 	{
 		question: 'Столицей Франции является город?',
 		options: ['Брюссель', 'Париж', 'Мадрид', 'Лиссабон'],
-		rightAnswer: 2,
+		rightAnswer: 1,
 	},
 	{
 		question: 'Столицей США является город?',
 		options: ['Голливуд', 'Вашингтон', 'Нью-Йорк', 'Бостон'],
-		rightAnswer: 2,
+		rightAnswer: 1,
 	},
 	{
 		question: 'Столицей Южной Кореи является город?',
 		options: ['Пхеньян', 'Тайбей', 'Сеул', 'Дакка'],
-		rightAnswer: 3,
+		rightAnswer: 2,
 	},
 ];
 
@@ -42,7 +42,7 @@ numberOfAllQuestions.innerHTML = questions.length;
 
 const load = () => {
 	question.innerHTML = questions[indexOfQuestion].question;
-	
+
 	option1.innerHTML = questions[indexOfQuestion].options[0];
 	option2.innerHTML = questions[indexOfQuestion].options[1];
 	option3.innerHTML = questions[indexOfQuestion].options[2];
@@ -55,7 +55,8 @@ const load = () => {
 let completeAnswers = [];
 
 const randomQuestion = () => {
-	let randomNumber = Math.floor(Math.random() + question.length);
+	let randomNumber = Math.floor(Math.random() * questions.length);
+	console.log(randomNumber);
 	let hitDuplicate = false;
 
 	if (indexOfPage == questions.length) {
@@ -64,7 +65,7 @@ const randomQuestion = () => {
 		if (completeAnswers.length > 0) {
 			completeAnswers.forEach((item) => {
 				if (item == randomNumber) {
-					hitDuplicate - true;
+					hitDuplicate = true;
 				}
 			});
 			if (hitDuplicate) {
@@ -75,17 +76,80 @@ const randomQuestion = () => {
 			}
 		}
 		if (completeAnswers.length == 0) {
-			indexOfQuestion - randomNumber;
+			indexOfQuestion = randomNumber;
 			load();
 		}
 	}
 	completeAnswers.push(indexOfQuestion);
 };
 
+const checkAnswer = el => {
+	if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+		el.target.classList.add('correct');
+		updateAnswerTracker('correct');
+		score++;
+	} else {
+		el.target.classList.add('wrong');
+		updateAnswerTracker('wrong');
+	}
+	disabledOptions();
+}
+
+for (option of optionElements) {
+	option.addEventListener('click', (e) => checkAnswer(e));
+}
+
+const disabledOptions = () => {
+	optionElements.forEach(item => {
+		item.classList.add('disabled');
+		if(item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+			item.classList.add('correct');
+		}
+	})
+}
+
+const enableOption = () => {
+	optionElements.forEach(item => {
+		item.classList.remove('disabled', 'correct', 'wrong');
+	})
+}
+
+const answerTracker = () => {
+	questions.forEach(() => {
+		const div = document.createElement('div');
+		answersTracker.appendChild(div);
+	})
+}
+
+const updateAnswerTracker = status => {
+	answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+}
+
+const validate = () => {
+	if(!optionElements[0].classList.contains('disabled')){
+		alert('Вам нужно выбрать один из вариантов ответа');
+	} else {
+		randomQuestion();
+		enableOption();
+	}
+}
+
 const quizOver = () => {
-	console.log('конец игры');
+	document.querySelector('.quiz-over-modal').classList.add('active');
+	correctAnswer.innerHTML = score;
+	numberOfAllQuestions2.innerHTML  = questions.length;
 };
 
+const tryAgain = () => {
+	window.location.reload();
+}
+
+btnTryAgain.addEventListener('click', tryAgain)
+btnNext.addEventListener('click', () => {
+	validate();
+})
+
 window.addEventListener('load', () => {
-	load();
+	randomQuestion();
+	answerTracker();
 });
